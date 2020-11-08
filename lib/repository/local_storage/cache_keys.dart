d8c007082044f676d80d9b269c8c4269b382748f
Prajval Prabhakar <prajvalprabhakar@Prajvals-MBP.domain.name>
2020-11-08 14:16:08 +0530

    [Prajval] Fixed cache keys

--- lib/repository/local_storage/cache_keys.dart
+++ lib/repository/local_storage/cache_keys.dart
@@ -3,7 +3,7 @@ class CacheKeys {
 
   static const String userQuestions = 'user_questions';
   static const int userQuestionsExpiryInHours = 6;
-  static const String questionDetails = 'questionDetails';
+  static const String questionDetails = 'question_details';
   static const String frequentlyAskedQuestions = 'frequently_asked_questions';
   static const int frequentlyAskedQuestionsExpiryInHours = 6;
 }
